792f3009cc69848e2f504b5ff37b43ffe2a62505
kvns07 <k.venkatnagsai@gmail.com>
2023-06-09 04:55:19 +0530

    commit

---
+++ lib/Screens/Intro/IntroPage1.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+class IntroPage1 extends StatelessWidget {
+  const IntroPage1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 18.0),
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Image.asset(
+                'Assets/shop.jpg',
+                fit: BoxFit.fitWidth,
+                width: 220.0,
+                alignment: Alignment.bottomCenter,
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                child: Column(
+                  children: <Widget>[
+                    Text("Online",
+                        style: TextStyle(
+                            fontSize: 50.0,
+                            fontWeight: FontWeight.w300,
+                            color: Color(0XFF3F3D56),
+                            height: 2.0)),
+                    Text(
+                      "Now do things Online",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          letterSpacing: 1.2,
+                          fontSize: 16.0,
+                          height: 1.3),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
---
+++ lib/Screens/on_boardingScreen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:sparks/Screens/Intro/IntroPage1.dart';
+import 'package:sparks/Screens/Intro/IntroPage2.dart';
+import 'package:sparks/Screens/Intro/IntroPage3.dart';
+class OnBoardScreen extends StatefulWidget {
+  const OnBoardScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoardScreen> createState() => _OnBoardScreenState();
+}
+
+class _OnBoardScreenState extends State<OnBoardScreen> {
+  bool onLastPage=false;
+  @override
+  Widget build(BuildContext context) {
+    final controller=PageController(
+      initialPage:0,
+    );
+    return Scaffold(
+
+      body: Stack(
+       children:[
+         PageView(
+           controller: controller,
+           onPageChanged: (index){
+             setState(() {
+               onLastPage=(index==2)?true:false;
+             });
+           },
+           scrollDirection: Axis.horizontal,
+           children: [
+             IntroPage1(),
+             IntroPage2(),
+             IntroPage3(),
+           ],
+  
+         ),
+         Container(
+           alignment: Alignment(0,0.75),
+             child: Row(
+               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+               children: [
+                 //skip
+                 onLastPage!=true ?
+                 GestureDetector(
+                      onTap:(){
+                        controller.jumpToPage(3);
+                      },
+                      child: Text(">>")
+                 ):
+                 GestureDetector(
+                     onTap:(){
+
+                     },
+                     child: Text("")
+                 ),
+
+                 SmoothPageIndicator(controller: controller, count: 3),
+
+                 //next
+                 onLastPage ?
+                 GestureDetector(
+                     onTap:(){
+                       // controller.jumpToPage(1);
+                     },
+                     child: Text("Done")
+                 ):
+                 GestureDetector(
+                     onTap:(){
+                       controller.nextPage(duration: Duration(milliseconds: 500), curve: Curves.easeIn);
+                     },
+                     child: Text(">")
+                 ),
+               ],
+             )
+         )
+       ]
+      )
+    );
+  }
+}
